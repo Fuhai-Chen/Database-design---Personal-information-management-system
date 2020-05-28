5c4e9bccb9b6d90377ff4ad912a6c7ddd2bbfb3c
Fuhai-Chen <60720508+Fuhai-Chen@users.noreply.github.com>
2020-05-28 19:02:39 +0800

    Upload development documents, script files ...
    开发文档（实验报告）、脚本文件、及原需求文件上传

---
+++ The SQL statement.sql
@@ -0,0 +1,74 @@
+/*==============================================================*/
+/* DataBase: 个人信息管理系统, 创建数据库，数据库名称为个人信息管理系统*/
+/*==============================================================*/
+create database `个人信息管理系统`
+go
+
+
+/*==============================================================*/
+/* Table: person, 用户信息表 */
+/*==============================================================*/
+CREATE TABLE `person` (
+  `姓名` varchar(20) NOT NULL,
+  `性别` varchar(6) NOT NULL DEFAULT '男',
+  `年龄` int(11) NOT NULL,
+  `出生日期` date NOT NULL,
+  `地址` varchar(45) NOT NULL,
+  `职业` varchar(45) NOT NULL,
+  `QQ` varchar(45) DEFAULT NULL,
+  `电话` varchar(45) NOT NULL,
+  PRIMARY KEY (`姓名`)	/* 主键约束 */
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户信息表'	/* 表格注释 */
+
+
+/*==============================================================*/
+/* Table: communication, 通讯录记录表 */
+/*==============================================================*/
+CREATE TABLE `communication` (
+  `用户名` varchar(45) NOT NULL,
+  `姓名` varchar(45) NOT NULL,
+  `电话` varchar(45) NOT NULL,
+  `工作地点` varchar(45) DEFAULT NULL,
+  `城市` varchar(45) DEFAULT NULL,
+  `备注` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`姓名`)	/* 主键约束 */
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='通讯录记录表'
+
+
+/*==============================================================*/
+/* Table: diary, 日记记录表 */
+/*==============================================================*/
+CREATE TABLE `diary` (
+  `用户名` varchar(45) NOT NULL,
+  `记录时间` datetime NOT NULL,
+  `时间` date DEFAULT NULL,
+  `地点` varchar(45) DEFAULT NULL,
+  `事件` varchar(100) NOT NULL,
+  PRIMARY KEY (`记录时间`)	/* 主键约束 */
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='日记记录表'
+
+
+/*==============================================================*/
+/* Table: financial, 财务记录表 */
+/*==============================================================*/
+CREATE TABLE `financial` (
+  `用户名` varchar(45) NOT NULL,
+  `项目` varchar(45) DEFAULT NULL,
+  `数额` double NOT NULL,
+  `添加时间` varchar(45) NOT NULL,
+  `收支类型` varchar(45) NOT NULL DEFAULT '支出',
+  PRIMARY KEY (`添加时间`)	/* 主键约束 */
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='财务记录表'
+
+
+/*==============================================================*/
+/* Table: memorandum, 备忘录记录表 */
+/*==============================================================*/
+CREATE TABLE `memorandum` (
+  `用户名` varchar(45) NOT NULL,
+  `记录时间` datetime NOT NULL,
+  `时间` datetime DEFAULT NULL,
+  `地点` varchar(45) DEFAULT NULL,
+  `事件` varchar(45) NOT NULL,
+  PRIMARY KEY (`记录时间`)	/* 主键约束 */
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci 
